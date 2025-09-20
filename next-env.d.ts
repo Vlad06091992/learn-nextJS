dc113f36fba05ad8512a8f6f96c7aa7d5a780955
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-09-20 16:13:07 +0500

    Настроил typescript для работы с SVG - 8.8

---
+++ next-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference path="./.next/types/routes.d.ts" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+
+declare module '*.svg' {
+    import { FC, SVGProps } from 'react'
+    const content: FC<SVGProps<SVGElement>>
+    export default content
+}
+
+declare module '*.svg?url' {
+    const content: any
+    export default content
+}
